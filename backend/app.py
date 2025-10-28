9fc4c161e862724081986b0305e2d004a4bd7ee5
Daniel Nellessen <dnellessen@gmx.de>
2025-10-28 17:22:54 +0100

    feat: get single anzeige and debug

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ def get_anzeigen():
     """
     GET /anzeigen
     
-    Fetch all records from the "anzeigen" table and attach a base64-encoded PNG image (if present) for each record.
+    Fetch all records from the "anzeigen" table and attach according base64-encoded PNG image (if present) for each record.
 
     Response
     --------
@@ -53,6 +53,7 @@ def get_anzeigen():
         for anzeige in all_anzeigen:
             anzeige["b64_bild"] = None
             try:
+                # Download according image and add to anzeige if present
                 image_bin: bytes = (
                     supabase.storage
                     .from_("images")
@@ -61,10 +62,70 @@ def get_anzeigen():
                 anzeige["b64_bild"] = "data:image/png;base64," + base64.b64encode(image_bin).decode('utf-8')
             except Exception:
                 pass
+            
     except Exception:
-        return {"error": "Error while trying to read from database", "response": response_table}, 400
+        return {"error": "Error while trying to read from database"}, 400
 
     return all_anzeigen, 200
+
+
+@app.get("/anzeigen/<uuid>")
+def get_anzeige_by_uuid(uuid: str):
+    """
+    GET /anzeigen
+    
+    Fetch specific record from the "anzeigen" table by uuid and attach according base64-encoded PNG image (if present).
+    
+    Parameters
+    ----------
+    uuid : str
+        UUID of the anzeige to get.
+
+    Response
+    --------
+    200 - anzeige object:
+    {
+        "uuid": (str),
+        "art": (str),
+        "aufgegeben_am": (str),
+        "titel": (str),
+        "beschreibung": (str),
+        "raum": (str),
+        "kategorie": (str | null),
+        "kontakt_email": (str | null),
+        "b64_bild": Optional[str]       # "data:image/png;base64,...", or None if not available
+    },
+    """
+    
+    try:
+        response_table = (
+            supabase.table("anzeigen")
+            .select("*")
+            .eq("uuid", uuid)
+            .execute()
+        )
+        
+        if len(response_table.data) == 0:
+            return {"error": "Anzeige does not exist"}, 404
+        
+        anzeige: list = response_table.data[0]
+        anzeige["b64_bild"] = None
+        
+        try:
+            # Download according image and add to anzeige if present
+            image_bin: bytes = (
+                supabase.storage
+                .from_("images")
+                .download(f"{anzeige['uuid']}.png")
+            )
+            anzeige["b64_bild"] = "data:image/png;base64," + base64.b64encode(image_bin).decode('utf-8')
+        except Exception:
+            pass
+        
+    except Exception:
+        return {"error": "Error while trying to read from database"}, 400
+
+    return anzeige, 200
 
 
 @app.post("/anzeigen")
@@ -73,7 +134,7 @@ def create_anzeige():
     POST /anzeigen
     
     Create a new record in the "anzeigen" table and optionally upload a PNG image to storage.
-    
+    `
     Request
     -------
     JSON body (application/json)
@@ -133,30 +194,27 @@ def create_anzeige():
             .execute()
         )
     except Exception:
-        return {"error": "Error while trying to add to database", "response": response_table}, 400
+        return {"error": "Error while trying to add to database"}, 400
     
     new_anzeige: dict = response_table.data[0]
     
     try:
         if bild_b64:
             # Upload image to storage with uuid as name
-            response_storage = (
-                supabase.storage
-                .from_("images")
+            supabase.storage.from_("images") \
                 .upload(
                     file=base64.b64decode(bild_b64.split("base64,")[1]),
                     path=f"{new_anzeige['uuid']}.png",
                     file_options={"content-type": "image/png"}
                 )
-            )
     except Exception:
-        return {"error": "Error while trying to upload image to database", "response": response_storage}, 400
+        return {"error": "Error while trying to upload image to database"}, 400
 
     return new_anzeige, 201
 
 
 @app.delete("/anzeigen/<uuid>")
-def delete_anzeige(uuid):
+def delete_anzeige(uuid: str):
     """
     DELETE /anzeigen/<uuid>
 
@@ -170,33 +228,18 @@ def delete_anzeige(uuid):
     Responses
     ---------
     200 - the deleted anzeige object:
-    {
-        "uuid": (str)
-        "art": (str),
-        "aufgegeben_am": (str),
-        "titel": (str),
-        "beschreibung": (str),
-        "raum": (str),
-        "kategorie": (str | null),
-        "kontakt_email": (str | null)
-    }
     """
     
     try:
         # Delete anzeige row from table
-        response_table = (
-            supabase.table("anzeigen")
-            .delete()
-            .eq("uuid", uuid)
-            .execute()
-        )
+        supabase.table("anzeigen").delete().eq("uuid", uuid).execute()
     except Exception:
-        return {"error": "Error while trying to delete from database", "response": response_table}, 400
+        return {"error": "Error while trying to delete from database"}, 400
         
     try:
         # Delete according image from storage
-        response_storage = supabase.storage.from_("images").remove([f"{uuid}.png"])
+        supabase.storage.from_("images").remove([f"{uuid}.png"])
     except Exception:
-        return {"error": "Error while trying to delete image from database", "response": response_storage}, 400
+        return {"error": "Error while trying to delete image from database"}, 400
 
-    return response_table.data, 200
+    return {}, 200
